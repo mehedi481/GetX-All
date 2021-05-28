777d74f1aa4db2bcf051d4f7ed1437dd85ee9479
Mehedi <mehedihasanmax95@gmail.com>
2021-05-28 16:43:57 +0600

    SnackBar

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("SnackBar"),
+      ),
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            // Snackbar has more property for customization
+            Get.snackbar("Title", "Your Messages",snackPosition: SnackPosition.BOTTOM);
+          },
+          child: Text("SnackBar"),
+        ),
+      ),
     );
   }
 }
